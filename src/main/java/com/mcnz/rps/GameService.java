0cf0f7aa3774c6b9a0c56fd25b1d580e0f0ec0ea
theonlydoublee <theonlydoublee@gmail.com>
2023-04-06 13:41:53 -0600

    Update GameService.java

--- src/main/java/com/mcnz/rps/GameService.java
+++ src/main/java/com/mcnz/rps/GameService.java
@@ -1,23 +1,57 @@
 package com.mcnz.rps;
+import java.util.Random;
 
 public class GameService {
 	
-
-	public GameSummary playRoshambo(String clientGesture) {
+	public String getRandom(String[] array) {
+		    int rnd = new Random().nextInt(array.length);
+		    return array[rnd];
+		}
 		
+	public GameSummary playRoshambo(String clientGesture) {
 		String result = "error";
-		if (clientGesture.equals("scissors")) {
-			result = "lose";
+		String[] options = {"rock", "paper", "scissors"};
+		Random ran = new Random();
+	        int randomNumber = ran.nextInt(options.length);
+	        String randomArrayElement = options[randomNumber];
+		String chosen = randomArrayElement;
+		String input = clientGesture;
+		if (input.equals("scissors")) {
+			if (chosen.equals("rock")){
+				result = "lose";
+			}
+			if (chosen.equals("paper")){
+				result = "win";
+			}
+			if (chosen.equals("scissors")){
+				result = "tie";
+			}			
 		}
-		if (clientGesture.equals("paper")) {
-			result = "win";
+
+		if (input.equals("paper")) {
+			if (chosen.equals("rock")){
+				result = "win";
+			}
+			if (chosen.equals("paper")){
+				result = "tie";
+			}
+			if (chosen.equals("scissors")){
+				result = "lose";
+			}
 		}
-		if (clientGesture.equals("rock")) {
-			result = "tie";
+
+		if (input.equals("rock")) {
+			if (chosen.equals("rock")){
+				result = "tie";
+			}
+			if (chosen.equals("paper")){
+				result = "lose";
+			}
+			if (chosen.equals("scissors")){
+				result = "win";
+			}
 		}
-		GameSummary gameSummary = new GameSummary(clientGesture, "rock", result);
+		GameSummary gameSummary = new GameSummary(clientGesture, chosen, result);
 		return gameSummary;
 	}
-
-}  
-  
+}
