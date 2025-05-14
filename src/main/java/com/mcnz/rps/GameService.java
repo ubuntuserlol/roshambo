6003e67e8a5374fa0f64a61f164b15bd30fe6a5b
ubuntuserlol <outerf1@gmail.com>
2025-05-14 09:50:01 -0600

    Update GameService.java

--- src/main/java/com/mcnz/rps/GameService.java
+++ src/main/java/com/mcnz/rps/GameService.java
@@ -1,23 +1,60 @@
 package com.mcnz.rps;
+import java.util.Random;
 
 public class GameService {
 	
 
 	public GameSummary playRoshambo(String clientGesture) {
-		
+        Random random = new Random();
+        String randomChoice;
+
+		int randomInt = random.nextInt(3);
+        if (randomInt == 0) {
+            randomChoice = "scissors";
+        }
+        else if (randomInt == 1) {
+            randomChoice = "paper";
+        }
+        else {
+            randomChoice = "rock";
+        }
+
 		String result = "error";
 		if (clientGesture.equals("scissors")) {
-			result = "lose";
+			if (randomChoice.equals("scissors")) {
+                result = "tie";
+            }
+            else if (randomChoice.equals("paper")) {
+                result = "win";
+            }
+            else {
+                result = "lose";
+            }
 		}
 		if (clientGesture.equals("paper")) {
-			result = "win";
+			if (randomChoice.equals("scissors")) {
+                result = "lose";
+            }
+            else if (randomChoice.equals("paper")) {
+                result = "tie";
+            }
+            else {
+                result = "win";
+            }
 		}
 		if (clientGesture.equals("rock")) {
-			result = "tie";
+			if (randomChoice.equals("scissors")) {
+                result = "win";
+            }
+            else if (randomChoice.equals("paper")) {
+                result = "lose";
+            }
+            else {
+                result = "tie";
+            }
 		}
-		GameSummary gameSummary = new GameSummary(clientGesture, "rock", result);
+		GameSummary gameSummary = new GameSummary(clientGesture, randomChoice, result);
 		return gameSummary;
 	}
 
 }  
-  
